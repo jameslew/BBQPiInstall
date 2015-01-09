72bf40e09749bab9b18c58c59019313cb0ee6e8d
Craig Jensen <crjens@hotmail.com>
2015-01-09 11:13:07 -0800

    fixes

--- install.sh
+++ install.sh
@@ -10,9 +10,19 @@ else
 	cd Adafruit_Python_ILI9341
 	sudo python setup.py install
 fi
-sudo npm install -g forever
-sudo cp node-server.sh /etc/init.d/
-cd /etc/init.d
-sudo chmod 755 node-server.sh
-sudo update-rc.d node-server.sh defaults
+#sudo npm install -g forever
+if [ -f /etc/init.d/node-server.sh ]; then
+	echo "node-server.sh already installed"
+else
+	sudo mv node-server.sh /etc/init.d/
+	cd /etc/init.d
+	sudo chmod 755 node-server.sh
+	sudo update-rc.d node-server.sh defaults
+	cd ~
+fi
+if [ -f ~/.bash_aliases ]; then
+	echo ".bash_aliases already installed"
+else
+	sudo mv .bash_aliases ~
+fi
 echo "finshed" 
